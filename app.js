7098821149a2d08167ac681c2651fde091efabc6
Mirtiloo1 <murilonunes2101@gmail.com>
2025-07-18 01:05:09 -0300

    edit2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Import Express.js
 const express = require("express");
+const axios = require("axios");
 
 // Create an Express app
 const app = express();
@@ -7,9 +8,11 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Set port and verify_token
+// Environment variables
 const port = process.env.PORT || 3000;
 const verifyToken = process.env.VERIFY_TOKEN;
+const whatsappToken = process.env.WHATSAPP_TOKEN;
+const phoneNumberId = process.env.PHONE_NUMBER_ID;
 
 // Route for GET requests
 app.get("/", (req, res) => {
@@ -29,9 +32,42 @@ app.get("/", (req, res) => {
 
 // Route for POST requests
 app.post("/", (req, res) => {
-  const timestamp = new Date().toISOString().replace("T", " ").slice(0, 19);
-  console.log(`\n\nWebhook received ${timestamp}\n`);
-  console.log(JSON.stringify(req.body, null, 2));
+  const body = req.body;
+
+  if (body.object === "whatsapp_business_account") {
+    const entry = body.entry[0];
+    const changes = entry.changes[0];
+    const value = changes.value;
+
+    if (value.messages) {
+      const message = value.messages[0];
+      const from = message.from;
+      const msg_body = message.text.body;
+
+      console.log(`Mensagem de ${from}: ${msg_body}`);
+
+      axios({
+        method: "POST",
+        url: `https://graph.facebook.com/v19.0/${phoneNumberId}/messages`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${whatsappToken}`,
+        },
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: {
+            body: "Olá!, recebi a sua mensagem: '" + msg_body + "'",
+          },
+        },
+      }).catch((error) => {
+        console.error(
+          "Falha ao enviar a mensagem:",
+          error.response ? error.response.data : error.message
+        );
+      });
+    }
+  }
   res.status(200).end();
 });
 
