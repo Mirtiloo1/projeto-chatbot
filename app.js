c96da45360ff4610e99fd206fa31cf5ce8eebd18
Mirtiloo1 <murilonunes2101@gmail.com>
2025-07-18 01:33:11 -0300

    modelo atualizado

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post("/", async (req, res) => {
 
       try {
         const completion = await openai.chat.completions.create({
-          model: "gpt-4o",
+          model: 'gpt-3.5-turbo',
           messages: [{ role: "user", content: msg_body }],
         });
 
